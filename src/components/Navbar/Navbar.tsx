043f106757d0c2121e9aa7f9db251fa4d89b9dc1
Karan Meghe <karanmeghe87@gmail.com>
2025-03-30 17:56:33 +0530

    Add: Build a navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import { HStack, Image } from '@chakra-ui/react';
+import logo from '../../assets/Logo/logo.webp';
+
+const Navbar = () => {
+  return (
+    <HStack>
+      <Image src={logo} boxSize='60px' />
+    </HStack>
+  );
+};
+
+export default Navbar;
