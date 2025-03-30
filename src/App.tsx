807b336039b4d303dcba81969d90521c070522c0
Karan Meghe <karanmeghe87@gmail.com>
2025-03-30 16:52:35 +0530

    style: Build Responsive UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 /** @format */
 
-import { Button, HStack } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 
 const App = () => {
   return (
-    <HStack>
-      <Button colorPalette='gray'>Click me</Button>
-      <Button colorPalette='red'>Click me</Button>
-    </HStack>
+    <Grid
+      templateColumns={{ base: '1fr', lg: '1fr 4fr' }}
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}>
+      <GridItem area='nav' bg='coral'>
+        Nav
+      </GridItem>
+
+      <GridItem area='aside' bg='green' display={{ base: 'none', lg: 'block' }}>
+        Aside
+      </GridItem>
+
+      <GridItem area='main' bg='gold'>
+        Main
+      </GridItem>
+    </Grid>
   );
 };
 export default App;
