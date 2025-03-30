fd35c52d9646062bf053a0b7e55ef0d0c1fe3e7d
Karan Meghe <karanmeghe87@gmail.com>
2025-03-30 18:52:15 +0530

    Add: Dark Mode

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,11 +2,13 @@
 
 import { HStack, Image } from '@chakra-ui/react';
 import logo from '../../assets/Logo/logo.webp';
+import { ColorModeButton } from '../ui/color-mode';
 
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack justify='space-between'>
       <Image src={logo} boxSize='60px' />
+      <ColorModeButton />
     </HStack>
   );
 };
