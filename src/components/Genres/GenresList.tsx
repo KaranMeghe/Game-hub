f27646346b0964b0b8c22d3eacb438ef3059e9dc
Karan Meghe <karanmeghe87@gmail.com>
2025-04-26 06:10:36 +0530

    refactor: selcted genre styling

--- src/components/Genres/GenresList.tsx
+++ src/components/Genres/GenresList.tsx
@@ -5,8 +5,9 @@ import { getCroppedImageUrl } from '@/services/services';
 import { HStack, List, Image, Button, Text, Center } from '@chakra-ui/react';
 import GenresSkelton from './GenresSkeleton';
 import { useFetchGenresQuery } from '@/Redux/api/genresApi';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setGenreId, setGenreName } from '@/Redux/Slices/filterSlice';
+import { RootState } from '@/Redux/store';
 
 const GenresList = () => {
   const { data, isLoading, isError } = useFetchGenresQuery();
@@ -14,6 +15,8 @@ const GenresList = () => {
   const genresCount = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
   // const { selectedGenre } = useFilterGames();
+
+  const { genreName } = useSelector((store: RootState) => store.filters);
 
   const handleFilteredGenresGames = (genreId: number, genreName: string) => {
     dispatch(setGenreName(genreName));
@@ -48,8 +51,8 @@ const GenresList = () => {
             <Button
               variant='ghost'
               fontSize='lg'
-              // fontWeight={selectedGenre === genre.name ? 'bold' : 'normal'}
-              // color={selectedGenre === genre.name ? 'green.400' : ''}
+              fontWeight={genreName === genre.name ? 'bold' : 'normal'}
+              color={genreName === genre.name ? 'green.400' : ''}
               onClick={() => handleFilteredGenresGames(genre?.id, genre?.name)}>
               {genre?.name}
             </Button>
